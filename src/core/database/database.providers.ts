5084aaf0af4573d1c8140bba9c35c69addaccdf5
prjppusr <karlkevinddomingo@gmail.com>
2021-09-30 00:17:39 +0800

    set dev env

--- src/core/database/database.providers.ts
+++ src/core/database/database.providers.ts
@@ -1,19 +1,32 @@
 import { Sequelize } from 'sequelize-typescript';
 import { USCities } from 'src/city/city.entity';
 import { USCounties } from 'src/county/county.entity';
+import { databaseConfig } from './database.config';
+import { SEQUELIZE, DEVELOPMENT, TEST, PRODUCTION } from './constants';
 
 export const databaseProviders = [
   {
-    provide: 'SEQUELIZE',
+    provide: SEQUELIZE,
     useFactory: async () => {
-      const sequelize = new Sequelize({
-        dialect: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'kevin',
-        database: 'geolocws_db',
-      });
+      let config;
+      switch (process.env.NODE_ENV) {
+        case DEVELOPMENT:
+          console.log(`START ${process.env.NODE_ENV}`);
+          config = databaseConfig.development;
+          break;
+        case TEST:
+          console.log(`START ${process.env.NODE_ENV}`);
+          config = databaseConfig.test;
+          break;
+        case PRODUCTION:
+          console.log(`START ${process.env.NODE_ENV}`);
+          config = databaseConfig.production;
+          break;
+        default:
+          console.log(`START ${process.env.NODE_ENV}`);
+          config = databaseConfig.development;
+      }
+      const sequelize = new Sequelize(config);
       sequelize.addModels([USCities, USCounties]);
       await sequelize.sync();
       return sequelize;
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
+  console.log(process.env.NODE_ENV);
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
