54691437e8bbeca26d31768748ada04d8d092b17
gkkdd <gt.karlkevinddomingo@gmail.com>
2021-10-12 23:33:57 +0800

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   console.log(process.env.NODE_ENV);
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('geolocws')
+    .setDescription('geolocws API description')
+    .setVersion('1.0')
+    .addTag('geolocation')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 
